61b930b66eeff00393970f9c38b54b70cbb08d6b
IslamHeshamEldesoky <islameldesoky@paymob.com>
2022-11-29 00:10:10 +0200

    - Populate UI with data

--- app/src/main/java/com/example/jakewhartonrepos/presentation/repo/RepoActivity.kt
+++ app/src/main/java/com/example/jakewhartonrepos/presentation/repo/RepoActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jakewhartonrepos.R
+import com.example.jakewhartonrepos.data.model.ReposItem
+import com.example.jakewhartonrepos.databinding.ActivityMainBinding
 import com.example.jakewhartonrepos.presentation.di.Injector
 import javax.inject.Inject
 
@@ -14,34 +18,34 @@ class RepoActivity : AppCompatActivity() {
 
 
     lateinit var viewModel: RepoViewModel
-
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var adapter: RepoAdapter
     @Inject
     lateinit var factory: RepoViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         (application as Injector).createRepoSubComponent().inject(this)
 
         viewModel = ViewModelProvider(this,this.factory).get(RepoViewModel::class.java)
 
-
-
-
-//        viewModel.getRepos(1,15).observe(this, Observer {
-//            Log.d("ITEM",it.get(1).name)
-//        })
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.getRepos(1,15)
         viewModel.repoLiveData.observe(this, Observer {
-            Log.d("ITEMS",it.get(1).name)
+            initView(it)
         })
     }
 
+    fun initView(reposList:List<ReposItem>){
+        binding.rvRepo.layoutManager = LinearLayoutManager(this)
+        adapter = RepoAdapter()
+        binding.rvRepo.adapter = adapter
+        adapter.setList(reposList)
+    }
 
 }
--- app/src/main/java/com/example/jakewhartonrepos/presentation/repo/RepoAdapter.kt
+++ app/src/main/java/com/example/jakewhartonrepos/presentation/repo/RepoAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.jakewhartonrepos.presentation.repo
 
-class RepoAdapter  {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jakewhartonrepos.R
+import com.example.jakewhartonrepos.data.model.ReposItem
+import com.example.jakewhartonrepos.databinding.RvRepoBinding
+
+class RepoAdapter() : RecyclerView.Adapter<RepoViewHolder>() {
+    private var reposList = ArrayList<ReposItem>()
+
+
+    fun setList(repos: List<ReposItem>) {
+        reposList.clear()
+        reposList.addAll(repos)
+    }
+
+    fun updateList(list: ArrayList<ReposItem>) {
+        reposList = list
+        notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: RvRepoBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.rv_repo, parent, false
+        )
+        return RepoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
+        holder.bind(reposList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return reposList.size
+    }
+}
+
+class RepoViewHolder(val binding: RvRepoBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(repo: ReposItem) {
+        binding.headerTextView3.text = repo.name
+    }
 }
